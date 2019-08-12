8a0f4842cb591911b6ab2d25b942b16696cc4844
devide <devide@126.com>
2019-08-12 14:25:31 +0800

    1

--- GoldKeyWebApi/Controllers/BaseInfo/BaseInfoController.cs
+++ GoldKeyWebApi/Controllers/BaseInfo/BaseInfoController.cs
@@ -8,6 +8,12 @@ using GK.Service;
 using GK.Model;
 using GK.Utils;
 using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
 namespace GoldKeyWebApi.Controllers.BaseInfo
 {
     /// <summary>
@@ -92,6 +98,55 @@ namespace GoldKeyWebApi.Controllers.BaseInfo
             catch (Exception e)
             {
                 return Json(new { code = 0, msg = e.Message });
+            }
+        }
+        [Route("uploadfile")]
+        [HttpPost]
+        public IHttpActionResult Uploadfile()
+        {
+            try
+            {
+                List<string> filenames = new List<string>();
+                string savedir = HttpContext.Current.Server.MapPath("~/Files");
+                if (!Directory.Exists(savedir)) Directory.CreateDirectory(savedir);
+                var files = HttpContext.Current.Request.Files.GetMultiple("file");
+                foreach (var file in files)
+                {
+                    String timestampFileName = Guid.NewGuid().ToString();
+                    string fileExt = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+                    string fullpath = Path.Combine(savedir, timestampFileName + fileExt);
+                    file.SaveAs(fullpath);
+                    filenames.Add(timestampFileName + fileExt);
+                }
+                return Json(new { code = 1, msg = "文件上传成功！",filenames= filenames });
+            }
+            catch (Exception e)
+            {
+                return Json(new { code = 0, msg = e.Message });
+            }
+        }
+        [Route("download")]
+        [HttpGet]
+        public HttpResponseMessage DownloadFile(string file)
+        {
+            try
+            {
+                var FilePath = System.Web.Hosting.HostingEnvironment.MapPath(@"~/files/"+ file);
+                var stream = new FileStream(FilePath, FileMode.Open);
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StreamContent(stream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = file
+                };
+                return response;
+            }
+            catch (Exception e)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NoContent);
+                response.Content = new StringContent(e.Message);
+                return response;
             }
         }
     }
